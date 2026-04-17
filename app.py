37d3ce9eab5bccaf42f47a5191e9a65b50f8149e
pranavi1252140025-creator <pranavi.1252140025@vit.edu>
2026-04-17 23:49:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -88,5 +88,5 @@ Risk Score: {risk_percent}%
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
